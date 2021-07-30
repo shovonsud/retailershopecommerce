8019b86fc744b7cd4d435805ddf14a76aac7a9a0
Sudipto Dasgupta <shovon.das.gupta@gmail.com>
2021-07-31 00:55:03 +0530

    update portal button hover effect

--- index.php
+++ index.php
@@ -10,17 +10,25 @@
     <link rel="icon" type="image/png" href="../../assets/images/icon.png">
     <link href='assets/style/style.css' rel='stylesheet'>
     <title>Portal</title>
+    <style>
+        #mgmtport:hover{
+            background-color: #FB3640 !important;
+        }
+        #custport:hover{
+            background-color: #FF67E7 !important;
+        }
+        </style>
 </head>
 
-<body style="background-color:#0A1931">
+<body style="background-color:#DEEDF0">
     <div class="container">
         <div class="position-absolute top-50 start-50 translate-middle">
             <div
                 class="justify-content-evenly text-center align-items-center gap-2 gap-lg-5 gap-md-3 d-sm-flex d-md-flex d-lg-inline-flex d-xl-inline-flex d-xxl-inline-flex">
-                <a class="btn btn-warning" target="_self" href="lib/orders/order.php" role="button">
+                <a class="btn btn-warning" id="custport" target="_self" href="lib/orders/order.php" role="button">
                     <h1>Customer Portal</h1>
                 </a><br><br>
-                <a class="btn btn-info" target="_self" href="lib/management" role="button">
+                <a class="btn btn-info" id="mgmtport" target="_self" href="lib/management" role="button">
                     <h1>Management Portal</h1>
                 </a>
             </div>
@@ -29,6 +37,9 @@
             <?php require_once "footer.php";?>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
