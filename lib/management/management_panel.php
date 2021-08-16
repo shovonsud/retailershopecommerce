90c983eb4f7bb3e8e35c45be81c404092a35cd2e
Sudipto Dasgupta <shovon.das.gupta@gmail.com>
2021-08-16 05:33:40 +0530

    jquery library linked

--- lib/management/management_panel.php
+++ lib/management/management_panel.php
@@ -18,6 +18,8 @@ if (isset($_SESSION['logged_in'])) {
     crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href='../../assets/style/style.css' rel='stylesheet'>
   <link rel="icon" type="image/png" href="../../assets/images/icon.png">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="../../assets/js/app.js"></script>
 </head>
 
