33bc418c77e87b1ebc9a64225a4da3b7c78b6d2b
Sudipto Dasgupta <shovon.das.gupta@gmail.com>
2021-09-16 08:57:28 +0530

    adding action center

--- lib/management/index.php
+++ lib/management/index.php
@@ -8,41 +8,131 @@ require_once "management_panel.php";
 </head>
 
 <body>
-    <div class="">
-        <h1 class="text-center p-4" style="background-color:#B6C867"><i>Dashboard</i></h1>
-        <div class="container pt-4 text-center">
-
-            <div class="p-5">
-                <div
-                    class="justify-content-center text-center align-items-center gap-2 gap-lg-5 gap-md-3 d-sm-flex d-md-inline-flex d-lg-inline-flex d-xl-inline-flex d-xxl-inline-flex">
-                    <a class="p-3 btn btn-dark" href="../products" style="">
-                        <?php
-$result = mysqli_query($db_conn, "SELECT COUNT(item_id) AS totalit from product");
-$data = mysqli_fetch_assoc($result);
-echo "Items: " . $data['totalit'] . "";
+    <h1 class="text-center p-4 " style="background-color:#B6C867"><i>Dashboard</i></h1>
+    <div class="container">
+        <h2 class="">Action Center</h2>
+        <hr>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="h4">Kiosk Orders</div>
+                <div class="tablerep table-responsive tableshadow" style="max-height:50%;min-width:20%">
+                    <table class="table table-hover table-striped">
+                        <thead class="table-info">
+                            <tr>
+                                <th scope="col">Order ID</th>
+                                <th scope="col">Customer Name</th>
+                                <th scope="col">Items</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody style="max-height:10%;">
+                            <?php
+$sql = "SELECT order_id,cust_name,order_cart,carttotal FROM orders WHERE order_attended=0 ORDER BY order_id ASC";
+$result = mysqli_query($db_conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $cartitems = json_decode($row['order_cart']);
+        ?>
+                            <tr>
+                                <td><?php echo $row['order_id']; ?></td>
+                                <td><?php echo $row['cust_name']; ?></td>
+                                <td class="">
+                                    <ol class="list-group list-group-numbered">
+                                        <?php
+foreach ($cartitems as $item_id) {
+            $sql2 = "SELECT item_name,variant from product where item_id=$item_id->itemid";
+            $result2 = $db_conn->query($sql2) or die($db_conn->error);
+            if ($result2->num_rows == 1) {
+                $row2 = $result2->fetch_assoc();
+                ?>
+                                        <li class="list-group-item d-flex justify-content-between align-items-start hoveritem rounded px-1"
+                                            style="font-size:.9rem !important;">
+                                            <div class="ms-2 me-auto">
+                                                <div class="fw-bold"><?php echo $row2["item_name"]; ?></div>
+                                                <div class=""><span
+                                                        class="p-1 text-center bg-info fw-bold rounded-circle"><?php echo $row2['variant']; ?></span>
+                                                </div>
+                                            </div>
+                                            <span
+                                                class="badge bg-success rounded-pill"><?php echo $item_id->qty; ?></span>
+                                        </li>
+                                        <?php
+}
+        }
+        ?>
+                                    </ol>
+                                </td>
+                                <td><i class="fas fa-rupee-sign me-1"> <?php echo $row['carttotal']; ?></i></td>
+                                <td class="">
+                                    <div class="d-flex gap-2">
+                                        <button type="button" class="btn btn-primary"
+                                            onclick="vieworder(<?php echo $row['order_id']; ?>)"><i
+                                                class="far fa-eye"></i></button>
+                                        <button type="button" class="btn btn-danger"><i
+                                                class="far fa-trash-alt"></i></button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
+}
+} else {
+    ?>
+                            <tr>
+                                <td colspan="5" class="bg-success text-white">No Action required</td>
+                            </tr>
+                            <?php
+}
 ?>
-                    </a><br><br>
-                    <a class="p-3 btn btn-dark" href="../category">
-                        <?php
-$result = mysqli_query($db_conn, "SELECT COUNT(id) AS totalcat from category");
-$data = mysqli_fetch_assoc($result);
-echo "Categories: " . $data['totalcat'] . "";
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="h4">Inventory status</div>
+                <div class="tablerep table-responsive tableshadow" style="max-height:50%;">
+                    <table class="table table-hover table-striped caption-top align-middle text-center">
+                        <thead class="table-danger">
+                            <tr>
+                                <th scope="col">Product ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+$sql2 = "SELECT item_id,item_name,variant,picturefilepath FROM product WHERE quantity=0 ORDER BY item_id ASC";
+$result2 = mysqli_query($db_conn, $sql2);
+if (mysqli_num_rows($result2) > 0) {
+    while ($row2 = mysqli_fetch_assoc($result2)) {
+        ?>
+                            <tr>
+                                <td><?php echo $row2['item_id']; ?></td>
+                                <td class="d-flex text-start"><img class="border"
+                                        src="<?php echo $row2['picturefilepath']; ?>" width="50" height="50" />
+                                    <div class="ms-2"><?php echo $row2['item_name']; ?></div>
+                                </td>
+                                <td class="fw-bold" style="color:red;">Out-of-Stock</td>
+                            </tr>
+                            <?php
+}
+} else {
+    ?>
+                            <tr>
+                                <td colspan="3" class="bg-success text-white">All products in stock</td>
+                            </tr>
+                            <?php
+}
 ?>
-                    </a><br><br>
-                    <a class="p-3 btn btn-dark" href="">
-                        <?php
-$result = mysqli_query($db_conn, "SELECT SUM(quantity) AS totalq from product");
-$data = mysqli_fetch_assoc($result);
-echo "Stock Quantity: " . $data['totalq'] . "";
-?>
-                    </a>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-    <div class="pt-5">
+    <!-- <div class="">
         <?php require_once "footer.php";?>
-    </div>
+    </div> -->
 </body>
 
 </html>
